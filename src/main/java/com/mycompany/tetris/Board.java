927d80f106cc9294342f82d833f701a70561d030
CarlosMartins00098 <carlosmartins00098@gmail.com>
2026-04-06 09:18:49 -0300

    add DRAWLINES

--- src/main/java/com/mycompany/tetris/Board.java
+++ src/main/java/com/mycompany/tetris/Board.java
@@ -9,7 +9,11 @@ import javax.swing.Timer;
 
 public class Board extends JPanel {
     
+    public static final int BOARD_WIDTH = 10;
+    public static final int BOARD_HEIGHT = 20;
+    public static final int BLOCK_SIZE = 30;
     private Timer looper;
+    private Color[][] board = new Color[BOARD_WIDTH][BOARD_HEIGHT];
     
     public Board(){
         looper = new Timer(500,new ActionListener() {
@@ -26,8 +30,18 @@ public class Board extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g); 
-        
+        g.setColor(Color.black);
+        g.fill3DRect(0, 0, getWidth(), getHeight(), true);
         g.draw3DRect(10, 10, 200, 200, true);
+        g.setColor(Color.white);
+        
+        for (int row = 0; row < BOARD_HEIGHT; row++) {
+            g.drawLine(0, BLOCK_SIZE * row, BLOCK_SIZE * BOARD_WIDTH,BLOCK_SIZE * row);
+        }
+        
+        for (int col = 0; col < BOARD_HEIGHT; col++) {
+            g.drawLine(0, 0, BLOCK_SIZE * BOARD_WIDTH, BLOCK_SIZE * BOARD_HEIGHT);
+        }
     }
     
 }
